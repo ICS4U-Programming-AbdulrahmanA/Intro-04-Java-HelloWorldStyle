590eb7352dd7a56b9aa89b3195bd16566d731b00
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2026-09-08 13:23:46 +0000

    hello world w style

--- HelloWorldStyle.java
+++ HelloWorldStyle.java
@@ -1,6 +1,20 @@
-public class HelloWorld {
-    public static void main(String[] args) {
+public final class HelloWorldStyle {
+
+    /**
+     * Prevent instantiation of utility class.
+     */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * The main method for the application.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(final String[] args) {
         System.out.println("hello with style");
         System.out.println("bye with style");
     }
 }
+
